254b5295c1e154e147a7e12ed0d535c523cc00c2
noonomyen <nomyen@noonomyen.com>
2025-04-17 16:44:43 +0700

    remove prefix path of static page with patch directory-listing script

--- static.sh
+++ static.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-[ -f ./static.sh ] && mv ./static.sh /tmp/static.sh
-
 command -v php >/dev/null 2>&1 || { echo "PHP not found"; exit 1; }
 
+mv ./static.sh /tmp/static.sh
+mv ./directory-listing.patch /tmp/directory-listing.patch
+
 curl -o /tmp/directory-listing.php https://raw.githubusercontent.com/caendesilva/php-directory-listing/master/directory-listing.php
+
+patch /tmp/directory-listing.php /tmp/directory-listing.patch 
 
 find . -type d ! -path "*/.git*" | while IFS= read -r dir; do
     echo "Running in $dir"
